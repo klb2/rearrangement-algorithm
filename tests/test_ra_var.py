952847e4d507f6428695de3ee0b2be6f723673f6
KLB <k.besser@tu-bs.de>
2021-03-16 11:34:27 +0100

    Add first pytest tests

--- tests/test_ra_var.py
+++ tests/test_ra_var.py
@@ -1,15 +1,23 @@
-import base
-import scratch
 from scipy import stats
-
-#qf = [stats.expon.ppf, stats.expon.ppf, stats.expon.ppf, stats.expon.ppf, stats.expon.ppf]
-#result_low, result_up = scratch.bounds_var(0., qf, method="upper", num_steps=50)
+import pytest
 
 
-qf = [stats.pareto(2, loc=-1).ppf]*3
-alpha = .5102
-results_up_under, results_up_over = scratch.bounds_var(1.-alpha, qf, method="upper", num_steps=50)
-results_low_under, results_low_over = scratch.bounds_var(1.-alpha, qf, method="lower", num_steps=50)
-print("Results")
-print(results_low_under[0], results_low_over[0])
-print(results_up_under[0], results_up_over[0])
+from rearrange_algorithm import bounds_var
+
+
+@pytest.mark.parametrize("alpha,expected", [(.5102, .5), (.1111, 2.0), (.16, 1.5)])
+def test_var_pareto_lower(alpha, expected):
+    """
+    From the paper "Computation of sharp bounds on the distribution of a
+    function of dependent risks" (Puccetti et al., 2012). (Table 1)
+    """
+    qf = [stats.pareto(2, loc=-1).ppf]*3
+    #alpha = .5102
+    results_low_under, results_low_over = bounds_var(
+            1.-alpha, qf, method="lower", num_steps=500)
+    var_under = results_low_under[0]
+    var_over = results_low_over[0]
+    #assert ((var_under <= var_over) and (.4998 < var_under < .5) and
+    #        (.5 < var_over < .51))
+    assert ((var_under <= var_over) and (expected*.98 < var_under < expected)
+            and (expected < var_over < expected*1.02))
